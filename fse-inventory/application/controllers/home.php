95b1eed9b0e3b0cbfed30fd07cf02f465e862bc9
callmejed <mwyatt15@gmail.com>
2017-12-05 18:10:11 -0500

    controller methods for each database table we'll be focusing on

--- fse-inventory/application/controllers/home.php
+++ fse-inventory/application/controllers/home.php
@@ -5,6 +5,11 @@ class home extends MY_Controller {
 
 	public function index()
 	{
+		
+	}
+
+	public function all_tools()
+	{
 		if($this->require_role('admin'))
 		{
 			$this->load->model('tools_model');
@@ -18,4 +23,38 @@ class home extends MY_Controller {
 			$this->template->load('home', $data);
 		}
 	}
+
+	public function all_office()
+	{
+		if($this->require_role('admin'))
+		{
+			$this->load->model('tools_model');
+			$all_tools = $this->tools_model->select_all_from_tools();
+			$tool_columns = $this->tools_model->get_columns();
+
+			$data['all_tools'] = $all_tools;
+			$data['tool_columns'] = $tool_columns;
+
+			$this->template->setAll('FSE Inventory');
+			$this->template->load('home', $data);
+		}
+	}
+
+	public function all_vehicle()
+	{
+		if($this->require_role('admin'))
+		{
+			$this->load->model('tools_model');
+			$all_tools = $this->tools_model->select_all_from_tools();
+			$tool_columns = $this->tools_model->get_columns();
+
+			$data['all_tools'] = $all_tools;
+			$data['tool_columns'] = $tool_columns;
+
+			$this->template->setAll('FSE Inventory');
+			$this->template->load('home', $data);
+		}
+	}
+
+
 }
