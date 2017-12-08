0d2b3f6b9fa68d063222b4d61c644855140fb51e
callmejed <mwyatt15@gmail.com>
2017-12-08 01:58:13 -0500

    Fix the login page, was missing 'name' attribute and wouldn't function

--- fse-inventory/application/views/authenticate/login.php
+++ fse-inventory/application/views/authenticate/login.php
@@ -12,7 +12,7 @@
 			<div class="row justify-content-center padding_medium">
 				<div class="col-6">
 					<p>
-						Login Error #' . $this->authentication->login_errors_count . '/' . config_item('max_allowed_attempts') . ': Invalid Username, Email Address, or Password.
+						Login Error #<?php echo $this->authentication->login_errors_count; ?>/<?php echo config_item('max_allowed_attempts'); ?>: Invalid Username, Email Address, or Password.
 					</p>
 					<p>
 						Username, email address and password are all case sensitive.
@@ -45,13 +45,13 @@
 				<div class="col-sm-3 form_styled_container">
 					<!-- Username Field -->
 					<label for="login_string" class="col-form-label">Username or Email</label>
-					<input type="text" class="form-control" id="login_string" placeholder="Username or email">
+					<input type="text" name="login_string" class="form-control" id="login_string" maxlength="255" placeholder="Username or email">
 
 					<br>
 
 					<!-- Password Field -->
 					<label for="login_pass" class="col-form-label">Password</label>
-				    <input class="form-control" type="password" id="login_pass" placeholder="Password"
+				    <input class="form-control" name="login_pass" type="password" id="login_pass" placeholder="Password"
 					    <?php if(config_item('max_chars_for_password') > 0)
 								echo 'maxlength="' . config_item('max_chars_for_password') . '"'; 
 						?> readonly="readonly" onfocus="this.removeAttribute('readonly');">
@@ -84,29 +84,26 @@
 
 	<?php
 
-		}
-		else
-		{
-			// EXCESSIVE LOGIN ATTEMPTS ERROR MESSAGE
-			echo '
-				<div style="border:1px solid red;">
-					<p>
-						Excessive Login Attempts
-					</p>
-					<p>
-						You have exceeded the maximum number of failed login<br />
-						attempts that this website will allow.
-					<p>
-					<p>
-						Your access to login and account recovery has been blocked for ' . ( (int) config_item('seconds_on_hold') / 60 ) . ' minutes.
-					</p>
-					<p>
-						Please use the <a href="/examples/recover">Account Recovery</a> after ' . ( (int) config_item('seconds_on_hold') / 60 ) . ' minutes has passed,<br />
-						or contact us if you require assistance gaining access to your account.
-					</p>
-				</div>
-			';
-		}
+	}
+	else
+	{
+		// EXCESSIVE LOGIN ATTEMPTS ERROR MESSAGE
+	?>
+			
+		<div class="row justify-content-center">
+			<div class="col-sm-6">
+				<p>Excessive Login Attempts</p>
+				<p>You have exceeded the maximum number of failed login attempts that this website will allow.</p>
+				<p>
+					Your access to login and account recovery has been blocked for <?php echo ((int)config_item('seconds_on_hold') / 60); ?> minutes.
+				</p>
+				<p>
+					Please use the <a href="/examples/recover">Account Recovery</a> after <?php echo ((int)config_item('seconds_on_hold') / 60); ?> minutes has passed or contact an administrator if you require assistance gaining access to your account.
+				</p>
+			</div>
+		</div>
+	<?php
+	}
 	?>
 
 </div>
