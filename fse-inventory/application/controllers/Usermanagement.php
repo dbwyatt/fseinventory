2dd28c6b90c62c2b7de3b11656fde7de3b71634c
Daniel Wyatt <dbwyatt23@gmail.com>
2018-01-01 10:34:19 -0700

    fix: Update use management name

--- fse-inventory/application/controllers/Usermanagement.php
+++ fse-inventory/application/controllers/Usermanagement.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class UserManagement extends MY_Controller {
+class Usermanagement extends MY_Controller {
 
 	public function index()
 	{
