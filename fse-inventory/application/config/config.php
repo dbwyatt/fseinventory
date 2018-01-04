242000631b824d13c47c32141b5d5193790706b3
Daniel Wyatt <dbwyatt23@gmail.com>
2018-01-03 23:31:24 -0700

    feat: Set environment correctly

--- fse-inventory/application/config/config.php
+++ fse-inventory/application/config/config.php
@@ -23,7 +23,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "http://" . $_SERVER['HTTP_HOST'] . "/fseinventory/";
+if (ENVIRONMENT === 'development') {
+	$config['base_url'] = "http://" . $_SERVER['HTTP_HOST'] . "/";
+}
+else {
+	$config['base_url'] = "http://" . $_SERVER['HTTP_HOST'] . "/fseinventory/";
+}
 
 /*
 |--------------------------------------------------------------------------
