f6382b50767a41be6f64f048dcde69c38a8f81cd
Daniel Wyatt <dbwyatt23@gmail.com>
2018-02-19 23:38:10 -0700

    feat: add office table toolbar

--- fseinventory/js/offices.js
+++ fseinventory/js/offices.js
@@ -1,24 +1,66 @@
-$(function() {
+const dataTable = require('./table');
 
-    let table = $('#office-table').DataTable({
-        pagingType: 'numbers',
+$(function() {
+    'use strict';
+
+    const tableOptions = {
         fixedHeader: true,
         autoFill: true,
         colReorder: true,
         rowReorder: true,
         responsive: true,
         select: true,
-        dom: 'Blfrtip',
+        // dom: 'Blfrtip',
         buttons: [
-            'copy',
-            'csv',
-            'excel',
+            {
+                text: 'Add',
+                action: function(e, dt, node, config) {
+                    dt.ajax.reload();
+                },
+                type: 'actions',
+                icon: 'plus-square'
+            },
+            {
+                text: 'Edit',
+                action: function(e, dt, node, config) {
+                    dt.ajax.reload();
+                },
+                type: 'actions',
+                icon: 'edit'
+            },
+            {
+                extend: 'copy',
+                text: 'Copy',
+                type: 'actions',
+                icon: 'copy'
+            },
+            {
+                extend: 'csv',
+                text: 'CSV',
+                type: 'exports',
+                icon: 'file'
+            },
+            {
+                extend: 'excel',
+                text: 'Excel',
+                type: 'exports',
+                icon: 'file-excel'
+            },
             {
                 extend: 'pdf',
-                orientation: 'landscape'
+                text: 'PDF',
+                orientation: 'landscape',
+                type: 'exports',
+                icon: 'file-pdf'
             },
-            'print'
+            {
+                extend: 'print',
+                text: 'Print',
+                type: 'exports',
+                icon: 'print'
+            }
         ]
-    });
-    
+    };
+    const table = dataTable.createTable('#office-table', tableOptions);
+    table.initializeButtons();
 });
